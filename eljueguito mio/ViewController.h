42d7eb0e198465eacf992e00c29718e27d047607
mac <mac@macs-MacBook-Pro.local>
2014-05-30 15:25:08 -0600

    Initial Commit

---
+++ eljueguito mio/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  eljueguito mio
+//
+
+//  Copyright (c) 2014 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
